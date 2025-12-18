541873987185db35fcead5a6e71a539be2f00188
Manish Raje <manishbraje@gmail.com>
2025-12-18 19:38:55 +0530

    fetches lat and long based on mood clicked

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,62 @@
 import { useEffect, useState } from "react";
 
 const Home = () => {
+
+
+  const moodToTags: Record<string, string[]> = {
+    work: ["cafe", "library"],
+    date: ["restaurant", "park"],
+    quick: ["fast_food"],
+    budget: ["food"],
+  };
+
   type Location = {
     lat: number;
     lng: number;
   };
 
   const [location, setLocation] = useState<Location | null>(null);
+  const [mood, setMood] = useState<string>("");
 
   const [error, setError] = useState("");
+
+  const fetchPlaces = async () => {
+    if (!location || !mood) return;
+
+    const tags = moodToTags[mood];
+
+    const query = `
+    [out:json];
+    (
+      ${tags
+        .map(
+          (tag) => `
+        node["amenity"="${tag}"](around:2000,${location.lat},${location.lng});
+      `
+        )
+        .join("")}
+    );
+    out;
+  `;
+
+    try {
+      const res = await fetch("https://overpass-api.de/api/interpreter", {
+        method: "POST",
+        body: query,
+      });
+
+      const data = await res.json();
+      console.log(data.elements);
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
+  useEffect(() => {
+    if (mood && location) {
+      fetchPlaces();
+    }
+  }, [mood]);
+
 
   useEffect(() => {
     if (!navigator.geolocation) {
@@ -45,6 +93,29 @@ const Home = () => {
       ) : (
         <p>Fetching your location...</p>
       )}
+
+      <h2>Select your mood</h2>
+
+      <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
+        <button onClick={() => setMood("work")} disabled={!location}>
+          ☕ Work
+        </button>
+
+        <button onClick={() => setMood("date")} disabled={!location}>
+          💑 Date
+        </button>
+
+        <button onClick={() => setMood("quick")} disabled={!location}>
+          🍔 Quick Bite
+        </button>
+
+        <button onClick={() => setMood("budget")} disabled={!location}>
+          💸 Budget
+        </button>
+      </div>
+
+      {mood && <p>Selected mood: {mood}</p>}
+
     </main>
   )
 }
